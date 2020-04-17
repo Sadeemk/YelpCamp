8c13cc76577f870eb641bb8a4a08f07cd2e52545
Sadeem Khan <imsadeem98@gmail.com>
2020-04-16 21:28:08 -0700

    added a new route to show more details of each campground

--- app.js
+++ app.js
@@ -10,10 +10,27 @@ app.set('view engine', 'ejs');
 //Schema setup for mongodb
 let campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 let Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: 'Sunny Steps',
+// 		image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg',
+// 		description: 'This is a campground on a step with amazing sunlight. No water, no bathrooms!'
+// 	},
+// 	function(err, campground) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log('NEWLY CREATED CAMPGROUND: ');
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 // let campgrounds = [
 // 	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
@@ -37,7 +54,7 @@ app.get('/campgrounds', (req, res) => {
 		if (err) {
 			console.log(err);
 		} else {
-			res.render('campgrounds', { campgrounds: allCampgrounds });
+			res.render('index', { campgrounds: allCampgrounds });
 		}
 	});
 });
@@ -46,7 +63,8 @@ app.post('/campgrounds', (req, res) => {
 	// get data from form and add to campgrounds array
 	let name = req.body.name;
 	let image = req.body.image;
-	let newCampground = { name: name, image: image };
+	let desc = req.body.description;
+	let newCampground = { name: name, image: image, description: desc };
 	Campground.create(newCampground, function(err, newlyCreated) {
 		if (err) {
 			console.log(err);
@@ -58,6 +76,18 @@ app.post('/campgrounds', (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
 	res.render('new');
+});
+
+// SHOW - shows more info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+	//find campgound with provided id
+	Campground.findById(req.params.id, (err, foundCampground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('show', { campground: foundCampground });
+		}
+	});
 });
 
 app.listen(3000, () => {
