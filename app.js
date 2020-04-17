7a400f82e0dc430ccd17d3b9f5dbee51578c0ba6
Sadeem Khan <imsadeem98@gmail.com>
2020-04-16 20:10:44 -0700

    removed hardcode for campgrounds and added campgrounds db instead

--- app.js
+++ app.js
@@ -15,39 +15,31 @@ let campgroundSchema = new mongoose.Schema({
 
 let Campground = mongoose.model('Campground', campgroundSchema);
 
-Campground.create(
-	{
-		name: 'Sunny Steps',
-		image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg'
-	},
-	function(err, campground) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('NEWLY CREATED CAMPGROUND: ');
-			console.log(campground);
-		}
-	}
-);
-
-let campgrounds = [
-	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
-	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
-	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' },
-	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
-	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
-	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' },
-	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
-	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
-	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' }
-];
+// let campgrounds = [
+// 	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
+// 	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
+// 	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' },
+// 	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
+// 	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
+// 	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' },
+// 	{ name: 'Weeping Woods', image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg' },
+// 	{ name: 'Sunny Steps', image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg' },
+// 	{ name: 'Lazy Lake', image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg' }
+// ];
 
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-	res.render('campgrounds', { campgrounds: campgrounds });
+	// get all campgrounds from db then render file
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds: allCampgrounds });
+		}
+	});
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -55,8 +47,13 @@ app.post('/campgrounds', (req, res) => {
 	let name = req.body.name;
 	let image = req.body.image;
 	let newCampground = { name: name, image: image };
-	campgrounds.push(newCampground);
-	res.redirect('/campgrounds');
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/campgrounds');
+		}
+	});
 });
 
 app.get('/campgrounds/new', (req, res) => {
